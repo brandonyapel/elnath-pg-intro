d13cbb3f16fc178d2109c446db904ddc8dec7727
Brandon Yapel <brandonyapel@gmail.com>
2017-11-21 10:17:36 -0600

    add basic server structure

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE shoes(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80),
+	cost INTEGER
+	);
+	
+INSERT INTO shoes(name,cost)
+VALUES 	('Red Wing',250),
+		('Puma Soliel V2', 40),
+		('Space Boots',10);
+
+SELECT * FROM shoes;
